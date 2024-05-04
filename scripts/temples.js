36d01da6102bc49b87b749dbcb195b3beea09fb6
shewandagns <shewandagns@outlook.com>
2024-05-05 00:32:04 +0300

    final update

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,63 +1,8 @@
-const hamMenu = document.querySelector(".ham-menu");
-const offScreenMenu = document.querySelector(".off-screen-menu");
-const offScreenMenuLinks = document.querySelectorAll(".off-screen-menu ul li a");
-const nav = document.querySelector("nav");
+//Hamburger Button and Navigation Menu
+const hamButton = document.querySelector("#menu");
+const navigation = document.querySelector("nav");
 
-// Function to toggle the visibility of the hamburger menu based on screen width
-function toggleHamMenu() {
-    if (window.innerWidth <= 640) {
-        hamMenu.style.display = "block";
-        nav.style.display = "none";
-    } else {
-        hamMenu.style.display = "none";
-        nav.style.display = "flex";
-    }
-}
-
-// Initial toggle based on screen width
-toggleHamMenu();
-
-// Event listener for window resize to toggle the hamburger menu
-window.addEventListener("resize", toggleHamMenu);
-
-// Event listener for hamburger menu click
-hamMenu.addEventListener("click", () => {
-    hamMenu.classList.toggle("active");
-    offScreenMenu.classList.toggle("active");
+hamButton.addEventListener("click",() => {
+    navigation.classList.toggle("open");
+    hamButton.classList.toggle("open");
 });
-
-// Close the off-screen menu when a link is clicked
-offScreenMenuLinks.forEach(link => {
-    link.addEventListener("click", () => {
-        hamMenu.classList.remove("active");
-        offScreenMenu.classList.remove("active");
-    });
-});
-
-// Update copyright year
-const currentYear = new Date().getFullYear();
-document.getElementById("currentyear").textContent = currentYear;
-
-// Update last modified date
-const lastModified = document.lastModified;
-document.getElementById("lastModified").textContent = "Last Modified: " + lastModified;
-
-
-/*
-const hamMenu = document.querySelector(".ham-menu");
-
-const offScreenMenu = document.querySelector(".off-screen-menu");
-
-hamMenu.addEventListener("click", () => {
-    hamMenu.classList.toggle("active");
-    offScreenMenu.classList.toggle("active");
-});
-
-// Update copyright year
-const currentYear = new Date().getFullYear();
-document.getElementById("currentyear").textContent = currentYear;
-
-//Update last modified date
-const lastModified = document.lastModified;
-document.getElementById("lastModified").textContent = "Last Modified: " + lastModified;
-*/
