fbb39bd730c83ff233f0835085041301a0708584
shewandagns <shewandagns@outlook.com>
2024-05-05 00:50:03 +0300

    Update

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,5 +1,5 @@
 //Hamburger Button and Navigation Menu
-const hamButton = document.querySelector("#menu");
+const hamButton = document.querySelector("#ham-menu");
 const navigation = document.querySelector("nav");
 
 hamButton.addEventListener("click",() => {
